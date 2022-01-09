dd017e823c70dc4c087d611192aeb09d76605984
Sohrab Osmany <sohrab1osmany@gmail.com>
2022-01-09 12:17:10 -0800

    test project

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+            $("#mycarousel").carousel({interval:2000});
+
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children('i').hasClass('fa-pause')) {
+
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children('i').removeClass('fa-pause');
+                    $("#carouselButton").children('i').addClass('fa-play');
+                }else if($("#carouselButton").children('i').hasClass('fa-play')) {
+
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children('i').removeClass('fa-play');
+                    $("#carouselButton").children('i').addClass('fa-pause');
+                }
+
+            });
+
+            $("#reserveTableButton").click(function(){
+                $('#reserveTableModal').modal('toggle');
+            });
+
+            $("#loginModalButton").click(function(){
+                $('#loginModal').modal('toggle');
+            });
+
+
+        });
